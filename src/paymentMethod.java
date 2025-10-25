565802c0e669c0c564033ea047cfb89a2140af07
klmnssi <klmnssidev@yahoo.com>
2025-10-25 17:40:32 +0300

    add logic functionanlty for payment method in the pymetMethod class

---
+++ src/paymentMethod.java
@@ -0,0 +1,13 @@
+public class paymentMethod {
+     private int amount;
+     private String name;
+     public paymentMethod(int amount, String name) {
+        this.amount = amount;
+     }
+     public int getAmount() {
+         return amount;
+     }
+     public void setAmount(int amount) {
+         this.amount = amount;
+     }
+}
